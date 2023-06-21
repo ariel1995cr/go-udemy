e391f465c908a00717fb1b3942590d3d15db31b0
Ariel Trivino <ariel.trivino@geopagos.com>
2023-06-21 14:50:47 -0300

    primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/arielt1995/godesde0
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/arielt1995/godesde0/variables"
+)
+
+func main() {
+	variables.MostrarEnteros()
+}
