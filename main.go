373734c3b237003756a8e96c29521e1bf8a58de7
Ariel Trivino <ariel.trivino@geopagos.com>
2023-06-21 15:12:56 -0300

    Commit de variables

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/arielt1995/godesde0/variables"
 )
 
 func main() {
 	variables.MostrarEnteros()
+	variables.RestoVariables()
+	estado, texto := variables.ConvertToText(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
